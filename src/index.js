a730235dbb77b5a8fe67acd69708f12243c79fe4
lisiurday <lisiurday@gmail.com>
2019-04-25 11:26:36 +0800

    feat: update

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const R = require('ramda')
+
+function watcher(fetch, Watcher) {
+  window.fetch = (...args) => {
+    const url = args[0]
+    const config = args[1]
+    const shouldWatch = url !== Watcher.global.origin+'/receiver'
+    const { emitReq, emitRes } = Watcher()
+    if (shouldWatch) {
+      const headers = config && config.headers || {}
+      const method = config && config.method || 'GET'
+      const query = parseIteratorData(new URL(url).searchParams)
+
+      let data = {}
+      if (config) {
+        const body = config.body
+        if (body instanceof FormData) {
+          data = parseIteratorData(body)
+        }
+        try {
+          data = JSON.parse(body)
+        } catch (e) {
+          data = {_body: body}
+        }
+      }
+
+      const params = R.mergeAll([query, data])
+      emitReq({headers, method, url, params}).catch(console.error)
+    }
+    const promise = fetch(...args)
+    if (shouldWatch) {
+      promise.then(res => {
+        const status = res.status
+        const data = res.json()
+        const headers = parseIteratorData(res.headers)
+        emitRes({status, headers, data}).catch(console.error)
+      })
+    }
+    return promise
+  }
+}
+
+function parseIteratorData(iteratorData) {
+  const data = {}
+  for (let pair of iteratorData.entries()) {
+    data[pair[0]] = pair[1]
+  }
+  return data
+}
+
+export default R.curry(watcher)
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'fetch-watcher.js',
+    path: path.resolve(__dirname, 'lib'),
+    library: 'fetchWatcher',
+    libraryTarget: 'commonjs2',
+    libraryExport: 'default'
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        include: [path.resolve(__dirname, 'src')],
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx', '.css']
+  },
+  devtool: 'source-map'
+}
